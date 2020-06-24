59b8ea1fad37f2debea4618ff4cbe227e2373f15
Gerasko-Vadim <vadim.gerasko99@gmail.com>
2020-06-24 20:00:37 +0600

    Fixed a bug in the nav bar

--- src/home/navBar/langu/lang.js
+++ src/home/navBar/langu/lang.js
@@ -11,9 +11,44 @@ const Lang =()=>{
 
   const [open,setOpen]=useState(false);
   const [flag,setFlag]=useState(Usa);
+  const [country,setCountry]= useState(['EN','RU','KG']);
 
   const toggleOpen = () => {
     setOpen(!open)
+  }
+  const Clickcountry=(event)=>{
+      const arr=[...country];
+      if(event.target.id==="1"){
+          for(var i=0;i<arr.length;i++){
+              if(i==1){
+                  arr.splice(i,1);
+                  arr.unshift(country[1]);
+                  setCountry([...arr])
+              }
+          }
+      }
+      if(event.target.id==="2"){
+        for(var i=0;i<arr.length;i++){
+            if(i==2){
+                arr.splice(i,1);
+                arr.unshift(country[2]);
+                setCountry([...arr])
+            }
+        }
+    }
+    switch(event.target.innerHTML){
+        case "EN":
+            setFlag(Usa);
+            break;
+        case "RU":
+            setFlag(Ru);
+            break;
+        case "KG":
+            setFlag(Kg);
+            break;
+    }
+  
+   
   }
 
 
@@ -28,14 +63,14 @@ const Lang =()=>{
           data-toggle="dropdown"
           aria-haspopup="true"
         >
-          EN
+          {country[0]}
         </sran>
         <div className={menuClass} aria-labelledby="dropdownMenuButton">
-          <a className="dropdown-item" href="#nogo">
-            RU
+          <a className="dropdown-item" id="1" href="#nogo" onClick={Clickcountry}>
+          {country[1]}
           </a>
-          <a className="dropdown-item" href="#nogo">
-          KG
+          <a className="dropdown-item" id="2" href="#nogo" onClick={Clickcountry}>
+          {country[2]}
           </a>
         </div>
       </div>
