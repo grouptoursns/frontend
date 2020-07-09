5de42e447f5198c27cd717f3c01dbada5feaec5b
Gerasko-Vadim <vadim.gerasko99@gmail.com>
2020-07-10 00:59:13 +0600

    Add page Result search but there are nuances

---
+++ src/result-search/result-card/result-card.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./result-card.css";
+import imgCard from "./img/Freme 42.png";
+import star1 from "./img/Star 6.png";
+import star2 from "./img/Star 6 (1).png";
+import mapPaint from "./img/map-pin.png";
+
+const ResultCard = () => {
+  const cards = {
+    img: imgCard,
+    stars: [star1, star1, star1, star1, star2],
+    paint: mapPaint,
+  };
+
+  return(
+<div>
+    
+</div>
+  )
+};
+export default ResultCard;
---
+++ src/result-search/result-search.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./result-search.css"
+
+const ResultSearch =()=>{
+    return(
+        <div className="container">
+            
+        </div>
+    )
+}
+export default ResultSearch;
