172147f942fa778899e9d9e2b31aeb5a602373b9
aktan24 <58247148+aktan24@users.noreply.github.com>
2020-07-10 05:03:44 +0600

    Added slider in tour page

--- src/tour-page/tour-form/pic-change/pic.js
+++ src/tour-page/tour-form/pic-change/pic.js
@@ -1,11 +1,35 @@
 import React from "react";
-import frame from './img/Frame7.png'
+import {Carousel} from 'react-responsive-carousel'
 import './pic.css'
+import 'react-responsive-carousel/lib/styles/carousel.css'
+import img1 from './img/a2.jpg'
+import img2 from './img/a5.jpg'
+
+// import img2 from './img/Thumbnail1.png'
+// import img3 from './img/Thumbnail2.png'
+
+
 
 const Pic = () =>{
     return(
-        <div className="container">
-                <img src={frame} alt="frame.icon"/>
+        <div className='slider'>
+            <Carousel autoPlay>
+                <div className='slider-carousel'>
+                    <img className='slider-carousel__img' src={img2} alt="pic.icon"/>
+                </div>
+                <div className='slider-carousel'>
+                    <img className='slider-carousel__img' src={img1} alt="pic.icon"/>
+                </div>
+                <div className='slider-carousel'>
+                    <img className='slider-carousel__img' src={img2} alt="pic.icon"/>
+                </div>
+                <div className='slider-carousel'>
+                    <img className='slider-carousel__img' src={img1} alt="pic.icon"/>
+                </div>
+                <div className='slider-carousel'>
+                    <img className='slider-carousel__img' src={img2} alt="pic.icon"/>
+                </div>
+            </Carousel>
         </div>
     )
 }
