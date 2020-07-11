0fb315cd0653fa8e94598db7880df2e3e6df617b
Gerasko-Vadim <vadim.gerasko99@gmail.com>
2020-07-11 13:02:45 +0600

    Add page Destinations and connected routing

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import React from "react";
 import NavBar from "./home/navBar/navBar.js";
 import Footer from "./home/footer/footer.js";
 import Home from "./home/home.js";
-import ResultSearch from "./result-search/result-search.js"
+import ResultSearch from "./result-search/result-search.js";
+import Destinations from "./destinations/destinations.js";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
@@ -14,6 +16,7 @@ const App = () => {
         <Switch>
         <Route path="/result_search" component={ResultSearch} />
         <Route exact path="/" component={Home} />
+        <Route path="/destinations" component={Destinations} />
         </Switch>
 
         <Footer />
---
+++ src/destinations/destinations.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./destinations.css";
+import card1 from "../home/best-destinations/img/CARD_DESTINATON.png";
+import card2 from "../home/best-destinations/img/CARD_DESTINATON(1).png";
+import card3 from "../home/best-destinations/img/CARD_DESTINATON(2).png";
+import card4 from "../home/best-destinations/img/CARD_DESTINATON(3).png";
+import card5 from "../home/best-destinations/img/CARD_DESTINATON(4).png";
+import card6 from "../home/best-destinations/img/CARD_DESTINATON(5).png";
+import card7 from "../home/best-destinations/img/CARD_DESTINATON(6).png";
+import card8 from "../home/best-destinations/img/CARD_DESTINATON(7).png";
+
+const Destinations = () => {
+  const pictures = [
+    { id: 1, img: card1 },
+    { id: 2, img: card2 },
+    { id: 3, img: card3 },
+    { id: 4, img: card4 },
+    { id: 5, img: card5 },
+    { id: 6, img: card6 },
+    { id: 7, img: card7 },
+    { id: 8, img: card8 },
+    { id: 9, img: card1 },
+    { id: 10, img: card2 },
+    { id: 11, img: card3 },
+    { id: 12, img: card4 },
+  ];
+  return (
+    <div className="container p1">
+      <div className="destion-title">
+        <span>Destinations</span>
+      </div>
+      <div className="destion-cards">
+        {pictures.map((item) => {
+          return (
+            <div className="cards" key={item.id}>
+              <img className="img" src={item.img} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default Destinations;
