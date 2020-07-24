beb27fb5787fd1a609f83aa1ec3e94d5a29cccff
aktan24 <58247148+aktan24@users.noreply.github.com>
2020-07-24 18:45:28 +0600

    Added normal validation and made authentication

--- src/componens/sign-in/form/user/user.js
+++ src/componens/sign-in/form/user/user.js
@@ -2,33 +2,66 @@ import React, { Component } from 'react';
 import '../form.css'
 
 
+const  initialState ={
+    isAuthenticated:false,
+    resData:'',
+    email:'',
+    password:'',
+    emailErr:'',
+    passwordErr:'',
+    check: false
+}
+
 export default class User extends Component {
 
     constructor(props){
         super(props);
-        this.state={
-            isAuthenticated:false,
-            resData:'',
-            text1:'',
-            text2:'',
-            checkbox: false
+        this.state={initialState}
+        this.Login = this.Login.bind(this)
+    }
+
+    handleChange = event =>{
+        const isCheckbox = event.target.type === "checkbox";
+        this.setState({
+            [event.target.name]: isCheckbox
+                ? event.target.checked
+                : event.target.value
+        })
+    }
+
+    validate = () =>{
+        let emailErr = '';
+        let passwordErr = '';
+
+        if(!this.state.password){
+            passwordErr = "Password cannot be blank"
         }
+
+        if (!this.state.email.includes("@")){
+            emailErr = 'Invalid email';
+        }
+        if (emailErr || passwordErr){
+            this.setState({emailErr, passwordErr})
+            return false;
+        }
+        return true;
+    };
+
+    handleSubmit = event =>{
+        event.preventDefault();
+        const isValid = this.validate();
+        if (isValid){
+            console.log(this.state);
+            //clear form
+            this.setState(initialState)
+        }
+
     }
 
-    change =(e)=>{
-        const { name, value, type, checked } = e.target
-
-        // if (type === 'checkbox'){
-        //     checked = ;
-        // }
-
-        this.setState({ [name]: value })
-    }
-
-    componentDidMount() {
+    Login() {
         const payLoad={
-            email: '{this.state.text1}',
-            password: '{this.state.text2}'
+            email: (this.state.email),
+            password: (this.state.password)
         }
         fetch('http:///161.35.199.172/api/users/login/',{
             method:'POST',
@@ -43,8 +76,7 @@ export default class User extends Component {
             this.setState({
             resData:data.token,
             isAuthenticated:true
-        });
-            
+            });
         },
         (error) => {
             console.log(error)
@@ -54,7 +86,6 @@ export default class User extends Component {
             })
         }
         );
-        
     }
 
 
@@ -70,37 +101,41 @@ export default class User extends Component {
         //         return(<p>No data was return</p>)
         //     }
         // }
-
         return (
-
             <div className="form">
-                <form className="form-cont">
+                <form className="form-cont" onSubmit={this.handleSubmit}>
                     <h2>Log in</h2>
                     <div>
                         <p className="form-cont__title">Login</p>
                         <input className="form-cont__input"
                         placeholder='Login'
-                        type="text"
-                               name="text1"
-                               value={this.state.text1}
-                               onChange={this.change}
+                        type="text" name="email" onChange={this.handleChange} value={this.state.email}
                         />
-                    </div>
+
+                        <div style={{color:"red", marginTop:"-7px", marginBottom:"5px"}}>
+                            {this.state.emailErr}
+                        </div>
+
+                        </div>
                     <div>
                         <p className="form-cont__title">Password</p>
                         <input className="form-cont__input"
                         placeholder='Password'
-                        type="password"
-                        name="text2"
-                               value={this.state.text2}
-                               onChange={this.change}
+                        type="password" name="password" onChange={this.handleChange} value={this.state.password}
                         />
+                        <div style={{color:"red", marginTop:"-7px", marginBottom:"5px"}}>
+                            {this.state.passwordErr}
+                        </div>
                     </div>
                     <div className="form-cont__check">
-                        <input className="form-cont__check-input" type="checkbox" name="checkbox" value={this.state.checkbox} onChange={this.change} />
+                        <input className="form-cont__check-input"
+                               type="checkbox"
+                               name="checkbox"
+                                onChange={this.handleChange} value={this.state.check}
+                        />
                         <p className="form-cont__check-title">Remember me</p>
                     </div>
-                    <button className="form-cont__btn">Login</button>
+                    <button className="form-cont__btn" type="submit" onClick={this.Login}>Login</button>
                     <a className="form-cont__link">Forgot your password?</a>
                 </form>
             </div>
