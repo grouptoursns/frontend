3fed733d43cf5e37d5a34612b7bd7892b226f127
Gerasko-Vadim <vadim.gerasko99@gmail.com>
2020-08-05 22:53:03 +0600

    Finished create authorization

--- src/componens/home/navBar/langu/lang.js
+++ src/componens/home/navBar/langu/lang.js
@@ -4,56 +4,128 @@ import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { connect } from "react-redux";
 
-const Lang = (props) => {
+const Lang = () => {
   const [open, setOpen] = useState(false);
-  const [token,setToken] = useState();
-  const [user,setUser]=useState();
-  const [company,setCompany]=useState();
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(false);
+  const [company, setCompany] = useState(false);
+  const [email, setEmail] = useState(1);
 
   const toggleOpen = () => {
     setOpen(!open);
   };
   useEffect(() => {
-    setToken(localStorage.getItem("login"));
+    setToken(localStorage.getItem("token"));
     setUser(localStorage.getItem("user"));
-    setCompany(localStorage.getItem("company"))
-  }, []);
-  console.log(token);
+    setCompany(localStorage.getItem("company"));
+    setEmail(localStorage.getItem("email"));
+  },[]);
+  console.log(user);
   const menuClass = `dropdown-menu${open ? " show" : ""}`;
-  return (
-    <div className="dropdown" onClick={toggleOpen}>
-      <span
-        className="btn  dropdown-toggle"
-        type="button"
-        id="dropdownMenuButton"
-        data-toggle="dropdown"
-        aria-haspopup="true"
-      >
-        Sign in
-      </span>
-      <div className={menuClass} aria-labelledby="dropdownMenuButton">
-        <Link className="dropdown-item" to="/sign-in">
-          Sign in
-        </Link>
-        <Link className="dropdown-item" to="/sign-up">
-          Sign up
-        </Link>
-      </div>
-    </div>
-  );
-};
 
+  if (localStorage.length !== 0) {
+    if (user === 'true') {
+      return (
+        <div className="dropdown" onClick={toggleOpen}>
+          <span
+            className="btn  dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+          >
+            {email}
+          </span>
+          <div className={menuClass} aria-labelledby="dropdownMenuButton">
+            <Link className="dropdown-item" to="/user-page">
+              Personal Area
+            </Link>
+            <Link className="dropdown-item" to="/sign-up">
+              logout
+            </Link>
+          </div>
+        </div>
+      );
+    }
+    if (company === 'true') {
+      return (
+        <div className="dropdown" onClick={toggleOpen}>
+          <span
+            className="btn  dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+          >
+          {email}
+          </span>
+          <div className={menuClass} aria-labelledby="dropdownMenuButton">
+            <Link className="dropdown-item" to="/sign-in">
+              Admin panel
+            </Link>
+            <Link className="dropdown-item" to="/sign-up">
+              logout
+            </Link>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="dropdown" onClick={toggleOpen}>
+          <span
+            className="btn  dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+          >
+            Sign in
+          </span>
+          <div className={menuClass} aria-labelledby="dropdownMenuButton">
+            <Link className="dropdown-item" to="/sign-in">
+              Sign in
+            </Link>
+            <Link className="dropdown-item" to="/sign-up">
+              Sign up
+            </Link>
+          </div>
+        </div>
+      );
+    }
+  }
+  if (localStorage.length === 0) {
+    return (
+      <div className="dropdown" onClick={toggleOpen}>
+        <span
+          className="btn  dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+        >
+          Sign in
+        </span>
+        <div className={menuClass} aria-labelledby="dropdownMenuButton">
+          <Link className="dropdown-item" to="/sign-in">
+            Sign in
+          </Link>
+          <Link className="dropdown-item" to="/sign-up">
+            Sign up
+          </Link>
+        </div>
+      </div>
+    );
+  }
+};
 
 const mapStateToProps = (state) => {
   return {
-  signIn:state.SuccessSignIn.signIn
+    signIn: state.SuccessSignIn.signIn,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-
-  };
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Lang);
--- src/componens/sign-in/form/user/user.js
+++ src/componens/sign-in/form/user/user.js
@@ -1,146 +1,164 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Home from "../../../home/home";
-import '../form.css'
+import "../form.css";
 import { connect } from "react-redux";
-import {signIn} from "../../../../actions/signIn"
+import { signIn } from "../../../../actions/signIn";
 
-
-const  initialState ={
-    isAuthenticated:false,
-    resData:'',
-    email:'',
-    password:'',
-    emailErr:'',
-    passwordErr:'',
-    checked: false,
-    userErr: '',
-    isUser: '',
-    isCompany: ''
-}
+const initialState = {
+  isAuthenticated: false,
+  resData: "",
+  email: "",
+  password: "",
+  emailErr: "",
+  passwordErr: "",
+  checked: false,
+  userErr: "",
+  isUser: "",
+  isCompany: "",
+};
 
 class User extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { initialState };
+    this.Login = this.Login.bind(this);
+  }
 
-    constructor(props){
-        super(props);
-        this.state={initialState}
-        this.Login = this.Login.bind(this)
+  handleChange = (event) => {
+    const isCheckbox = event.target.type === "checkbox";
+    this.setState({
+      [event.target.name]: isCheckbox
+        ? event.target.checked
+        : event.target.value,
+    });
+  };
+
+  validate = () => {
+    let emailErr = "";
+    let passwordErr = "";
+    let userErr = "";
+
+    if (this.state.resData) {
+      userErr = "No such user exists";
     }
 
-    handleChange = event =>{
-        const isCheckbox = event.target.type === "checkbox";
-        this.setState({
-            [event.target.name]: isCheckbox
-                ? event.target.checked
-                : event.target.value
-        })
+    if (!this.state.password) {
+      passwordErr = "Password cannot be blank";
     }
 
-    validate = () =>{
-        let emailErr = '';
-        let passwordErr = '';
-        let userErr = '';
+    if (!this.state.email.includes("@")) {
+      emailErr = "Invalid email";
+    }
+    if (emailErr || passwordErr || userErr) {
+      this.setState({ emailErr, passwordErr, userErr });
+      return false;
+    }
 
+    return true;
+  };
 
-        if(this.state.resData){
-            userErr = "No such user exists"
-        }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const isValid = this.validate();
+    if (isValid) {
+      console.log(this.state);
+      //clear form
+      // this.setState(initialState)
+    }
+  };
 
-        if(!this.state.password){
-            passwordErr = "Password cannot be blank"
-        }
-
-        if (!this.state.email.includes("@")){
-            emailErr = 'Invalid email';
-        }
-        if (emailErr || passwordErr || userErr){
-            this.setState({emailErr, passwordErr, userErr})
-            return false;
-        }
-
-        return true;
+  Login() {
+    const payLoad = {
+      email: this.state.email,
+      password: this.state.password,
     };
+    this.props.signIn(payLoad);
+  }
 
-    handleSubmit = event =>{
-        event.preventDefault();
-        const isValid = this.validate();
-        if (isValid){
-            console.log(this.state);
-            //clear form
-            // this.setState(initialState)
-        }
+  render() {
+    return (
+      <div className="form">
+        <form className="form-cont" onSubmit={this.handleSubmit}>
+          <h2>Login</h2>
+          <div>
+            <p className="form-cont__title">Email</p>
+            <input
+              className="form-cont__input"
+              placeholder="Email"
+              type="text"
+              name="email"
+              onChange={this.handleChange}
+              value={this.state.email}
+            />
 
-    }
-
-    Login() {
-        const payLoad={
-            email: (this.state.email),
-            password: (this.state.password)
-        }
-        this.props.signIn(payLoad)
-
-    }
-
-
-    render() {
-        return (
-            <div className="form">
-                <form className="form-cont" onSubmit={this.handleSubmit}>
-                    <h2>Login</h2>
-                    <div>
-                        <p className="form-cont__title">Email</p>
-                        <input className="form-cont__input"
-                        placeholder='Email'
-                        type="text" name="email" onChange={this.handleChange} value={this.state.email}
-                        />
-
-                        <div style={{color:"red", marginTop:"-7px", marginBottom:"5px"}}>
-                            {this.state.emailErr}
-                        </div>
-
-                        </div>
-                    <div>
-                        <p className="form-cont__title">Password</p>
-                        <input className="form-cont__input"
-                        placeholder='Password'
-                        type="password" name="password" onChange={this.handleChange} value={this.state.password}
-                        />
-                        <div style={{color:"red", marginTop:"-7px", marginBottom:"5px"}}>
-                            {this.state.passwordErr}
-                        </div>
-                    </div>
-                    <div className="form-cont__check">
-                        <input className="form-cont__check-input"
-                               type="checkbox"
-                               name="checkbox"
-                               onChange={this.handleChange.bind(this)}
-                               value={this.state.checked}
-                        />
-                        <p className="form-cont__check-title">Remember me</p>
-                    </div>
-                    <button className="form-cont__btn" type="submit" onClick={this.Login}>Login</button>
-                    <a className="form-cont__link">Forgot your password?</a>
-
-                    <div style={{color:"red",fontWeight:"bold", marginTop:"-7px", marginBottom:"5px"}}>
-                        {this.state.userErr}
-                    </div>
-
-                </form>
+            <div
+              style={{ color: "red", marginTop: "-7px", marginBottom: "5px" }}
+            >
+              {this.state.emailErr}
             </div>
-        )
-    }
+          </div>
+          <div>
+            <p className="form-cont__title">Password</p>
+            <input
+              className="form-cont__input"
+              placeholder="Password"
+              type="password"
+              name="password"
+              onChange={this.handleChange}
+              value={this.state.password}
+            />
+            <div
+              style={{ color: "red", marginTop: "-7px", marginBottom: "5px" }}
+            >
+              {this.state.passwordErr}
+            </div>
+          </div>
+          <div className="form-cont__check">
+            <input
+              className="form-cont__check-input"
+              type="checkbox"
+              name="checkbox"
+              onChange={this.handleChange.bind(this)}
+              value={this.state.checked}
+            />
+            <p className="form-cont__check-title">Remember me</p>
+          </div>
+          <button className="form-cont__btn" type="submit" onClick={this.Login}>
+            Login
+          </button>
+          <a className="form-cont__link">Forgot your password?</a>
+
+          <div
+            style={{
+              color: "red",
+              fontWeight: "bold",
+              marginTop: "-7px",
+              marginBottom: "5px",
+            }}
+          >
+            {this.state.userErr}
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 const mapStateToProps = (state) => {
-    return {
-
-    };
+  return {
+    signInData: state.SuccessSignIn.signIn,
   };
-  
-  const mapDispatchToProps = (dispatch) => {
-    return {
-      signIn: (data) => dispatch(signIn(data)),
-    };
-  };
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(User);
+};
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signIn: (data) => dispatch(signIn(data)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(User);
