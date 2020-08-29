ea9f830cad6c646536cc95147a6007add661f050
Gerasko-Vadim <vadim.gerasko99@gmail.com>
2020-08-29 23:30:05 +0600

    Fixed bug2.0

--- src/componens/result-search/result-card/result-card.js
+++ src/componens/result-search/result-card/result-card.js
@@ -81,7 +81,7 @@ const ResultCard = (props) => {
     searchTour = [...props.tourSearch];
     tourActivity = [...props.tourActivity];
     filterTour1 = [...props.tourFilter];
-    console.log(tour);
+    console.log(searchTour);
   }
   const clickDetailsContact = (id) => {
     props.detailsTour(id);
@@ -93,7 +93,7 @@ const ResultCard = (props) => {
         {searchTour.map((item) => {
           return (
             <div className="result-li" key={item.id}>
-              <img alt="" src={item.main_image ? `http://161.35.199.172${item.main_image}` :noImage} />
+              <img alt="" src={item.main_image ? item.main_image :noImage} />
               <div className="description-tours">
                 <h1 className="title-tours">{item.name}</h1>
                 <div className="description">
