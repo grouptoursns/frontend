f3acc52f12291fba2d299656b9d4b52b2e97e994
Gerasko-Vadim <vadim.gerasko99@gmail.com>
2020-09-02 16:37:30 +0600

    fixed logo

--- src/componens/home/navBar/logo/logo.js
+++ src/componens/home/navBar/logo/logo.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import logo from "./img/pawtects_logo_big.png"
 
 const Logo = ()=>{
 return(
-    <img  style={{"width":"80px"}} className="logo" />
+    <img  style={{"width":"80px"}} src={logo} className="logo" />
 );
 };
 export default Logo;
