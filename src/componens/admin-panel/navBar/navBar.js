22155460cad6e413528728f075d34317e6747f24
Gerasko-Vadim <vadim.gerasko99@gmail.com>
2020-09-02 16:40:53 +0600

    fixed bug admin panel

--- src/componens/admin-panel/navBar/navBar.js
+++ src/componens/admin-panel/navBar/navBar.js
@@ -1,11 +1,12 @@
 import React from "react"
 import "./navBar.css"
+import logo from "./img/pawtects_logo_big.png"
 
 const NavBar=()=>{
     return(
         <div className="admin-navBar">
             <div className="adminText">
-                <span>ALL TOURS</span>
+                <img  alt="img" className="logo-site" src={logo}/>
 
             </div>
         </div>
